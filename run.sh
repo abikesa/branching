5ce168b2d5cce2ec36709b6d6e3cd4c9ca6dbd2a
abikesa <144293697+abikesa@users.noreply.github.com>
2025-04-28 16:58:24 -0400

    no branching anymore

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python dumbo/kitabo/ensi/branch/plant_ipynb_flicks.py --wild --limit 3
-for i in {1..9}; do git branch thirdrail-$i; done
+# for i in {1..9}; do git branch thirdrail-$i; done
 git push --all
